712fa16325d3686c7658c3bc1c857d42ec6d6ea6
Tamás Tóth <tamas516@gmail.com>
2023-06-07 14:17:40 +0200

    React frontend

--- webprojekt-notes/webpr-notes/src/components/fetching.tsx
+++ webprojekt-notes/webpr-notes/src/components/fetching.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState, FC } from 'react';
 import axios from 'axios';
 import { Note, NoteColor, NoteItem } from './pages/note-list-page/types';
-import { Text } from "@chakra-ui/react"
+import { Box, Flex, Text } from "@chakra-ui/react"
+import { NoteTitle } from './notes-title';
+import { TodoList } from './todo-list';
 
 const Fetching: FC = () => {
     const [ notes, setNotes ] = useState<Note>();
@@ -9,14 +11,24 @@ const Fetching: FC = () => {
     useEffect(() => {
        axios.get("/note")
        .then(res => {
-        setNotes(res.data)
+        setNotes(res.data.note)
        });
     }, []);
     
+    if(!notes)
+    {
+        return null;
+    }
 
     return (
-        <Text as="h2">{JSON.stringify(notes)}</Text>
-    )
+        <Flex justifyContent="center">
+            <Box backgroundColor={notes.color} maxW="sm">
+                <NoteTitle title={notes.title}/>
+                <TodoList items={notes.items} />
+            </Box>
+        </Flex>
+        
+        )
 };
 
 export default Fetching
--- webprojekt-notes/webpr-notes/src/components/todo-item.tsx
+++ webprojekt-notes/webpr-notes/src/components/todo-item.tsx
@@ -1,13 +1,19 @@
 import React, { FC } from "react";
 import { Note, NoteItem } from "./pages/note-list-page/types";
-import { Text } from "@chakra-ui/react";
+import { Checkbox, Flex, FormControl, Icon, IconButton, Input, Text } from "@chakra-ui/react";
 
 export interface TodoItemProps{
     item: NoteItem
 }
 
 export const TodoItem: FC<TodoItemProps> = ({item}) => {
+    
     return (
-        <Text as="h4">{item.label}</Text>
+        <Flex>
+            <Checkbox>
+                <Text as="h4">{item.label}</Text>
+            </Checkbox>
+        </Flex>
+       
     )
 };
